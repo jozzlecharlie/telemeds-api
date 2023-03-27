b19d838be619901072622d27574b41133fa7fcdc
Jozzle Charlie Salacup <jozzlecharlie@gmail.com>
2023-03-27 17:34:05 +0800

    first commit

---
+++ database/migrations/2023_03_27_072624_create_hperson_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('hperson', function (Blueprint $table) {
+            $table->id();
+            $table->string('lname');
+            $table->string('fname');
+            $table->string('mname');
+            $table->string('suffix')->nullable();
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('hperson');
+    }
+};
