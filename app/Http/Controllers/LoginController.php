7ebb6b8b770bca31051c67013166a73b4b68b346
Jozzle Charlie Salacup <jozzlecharlie@gmail.com>
2023-03-28 15:49:08 +0800

    Update LoginController

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -11,17 +11,32 @@ class LoginController extends Controller
     public function __invoke(Request $request): \Illuminate\Http\JsonResponse
     {
         $validated = $request->validate([
-            'email' => 'required',
+            'email' => 'required|email',
             'password' => 'required',
         ]);
 
         $user = User::where('email', $validated['email'])->first();
+
+        // if(!$user){
+        //     return response()->json([
+        //         'message' => 'User not found'
+        //     ], 404);
+        // }
 
         if ($user && Hash::check($validated['password'], $user->password)) {
             $user->token = $user->createToken('auth_token')->plainTextToken;
             return response()->json($user);
         }
 
-        return response()->json(['error' => 'Invalid credentials'], 401);
+        if(!Hash::check($validated['password'],$user->password)){
+                 return response()->json(['message' => 'Invalid password'], 401);
+        }
+
+        //  return response()->json(['error' => 'Invalid credentials'], 401);
+        $token = $user->createToken('auth_token')
+            ->plainTextToken;
+
+        $user->token = $token;
+        return response()->json($user);
     }
 }
