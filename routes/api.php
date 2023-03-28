06a50d4bfa171e078e3760c9f63c95be92e2e5c6
Jozzle Charlie Salacup <jozzlecharlie@gmail.com>
2023-03-28 15:29:10 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -21,14 +21,26 @@ use App\Http\Controllers\PatientController;
 Route::post('/auth/login', LoginController::class);
 Route::get('/services', ServiceController::class);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+ Route::middleware('auth:sanctum')->group(function () {
+//     Route::get('/user', function (Request $request) {
+//         return $request->user();
+//       });
+ 
+     Route::get('/patients', PatientController::class);
+ });
+
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', function (Request $request) {
+    Route::get('/auth/user', function (Request $request) {
         return $request->user();
-      });
- 
-    Route::get('/patients', PatientController::class);
+    });
+
+    Route::post('/auth/logout', function (Request $request) {
+        $request->user()->currentAccessToken()->delete();
+        return response()->json(['message' => 'Logged out']);
+    });
 });
